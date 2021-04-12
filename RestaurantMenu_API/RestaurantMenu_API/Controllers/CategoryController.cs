4f6b83c773f36303469bae462e03fcd1f78d980a
Angel Servellon <maritzafunes098@outlook.es>
2021-04-11 21:35:23 -0600

    Creation of Controller to make methods GET, POST, PUT and DELETE for table Category for the API

---
+++ RestaurantMenu_API/RestaurantMenu_API/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using RestaurantMenu_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace RestaurantMenu_API.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Category c)
+        {
+            HttpPostedFileBase FileBase = Request.Files[0];
+            //HttpFileCollectionBase collectionBase = Request.Files;
+
+            WebImage image = new WebImage(FileBase.InputStream);
+
+            c.Image = image.GetBytes();
+
+            if (ModelState.IsValid)
+            {
+                using (var db = new RestaurantContext())
+                {
+                    db.Category.Add(c);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
