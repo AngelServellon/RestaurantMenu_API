5daa95aa0c5285d4c864e57dceab250584f09ff0
Angel Servellon <maritzafunes098@outlook.es>
2021-04-11 22:17:36 -0600

    Creation of API Controller to make methods GET, POST, PUT and DELETE for table Tag for the API

---
+++ RestaurantMenu_API/RestaurantMenu_API/Controllers/APITagController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantMenu_API.Models;
+
+namespace RestaurantMenu_API.Controllers
+{
+    public class APITagController : ApiController
+    {
+        private RestaurantContext db = new RestaurantContext();
+
+        // GET: api/APITag
+        public IQueryable<Tag> GetTag()
+        {
+            return db.Tag;
+        }
+
+        // GET: api/APITag/5
+        [ResponseType(typeof(Tag))]
+        public IHttpActionResult GetTag(int id)
+        {
+            Tag tag = db.Tag.Find(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tag);
+        }
+
+        // PUT: api/APITag/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTag(int id, Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tag.Id_Tag)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/APITag
+        [ResponseType(typeof(Tag))]
+        public IHttpActionResult PostTag(Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tag.Add(tag);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tag.Id_Tag }, tag);
+        }
+
+        // DELETE: api/APITag/5
+        [ResponseType(typeof(Tag))]
+        public IHttpActionResult DeleteTag(int id)
+        {
+            Tag tag = db.Tag.Find(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            db.Tag.Remove(tag);
+            db.SaveChanges();
+
+            return Ok(tag);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TagExists(int id)
+        {
+            return db.Tag.Count(e => e.Id_Tag == id) > 0;
+        }
+    }
+}
